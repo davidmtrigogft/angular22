14b1be8a2828f819d235994004328a104acb9e01
David Martínez <david.martinez@gft.com>
2026-07-20 17:46:47 +0200

    added signal form

--- src/app/modules/create-user/create-user.ts
+++ src/app/modules/create-user/create-user.ts
@@ -1,8 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { email, form, FormField, required } from '@angular/forms/signals';
+import { User } from '../../shared';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.html',
   styleUrl: './create-user.scss',
+  imports: [FormField, FormsModule],
 })
-export class CreateUser {}
+export class CreateUser {
+  private readonly model = signal<User>({
+    id: 0,
+    name: '',
+    email: '',
+    avatar: '',
+  });
+
+  protected readonly userForm = form(this.model, (path) => {
+    required(path.name);
+    required(path.email);
+    email(path.email);
+  });
+
+  protected onSubmit(): void {
+    console.log('click en el submit');
+    console.log(this.userForm().value());
+    console.log(this.model());
+  }
+
+  protected onAvatarSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const file = input.files?.[0];
+
+    if (!file) {
+      return;
+    }
+
+    this.userForm.avatar?.().value.set(file.name);
+  }
+}
